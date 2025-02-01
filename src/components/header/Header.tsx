0790dce936f80cf5162409497a277cef3228b361
Iain Maitland <iain.maitland@wayflyer.com>
2025-02-01 14:13:27 +0000

    Hot key toggle colour scheme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import { Container, Group, Image, Button, Text } from "@mantine/core";
 import { useMantineColorScheme, useComputedColorScheme } from "@mantine/core";
 import { IconSun, IconMoon } from "@tabler/icons-react";
 import classes from "./Header.module.css";
+import { useHotkeys } from "@mantine/hooks";
 
 export function Header() {
   const { setColorScheme } = useMantineColorScheme();
@@ -9,27 +10,29 @@ export function Header() {
   const toggleColorScheme = () =>
     setColorScheme(computedColorScheme === "dark" ? "light" : "dark");
 
+  useHotkeys([["mod+L", () => toggleColorScheme()]]);
+
   return (
     <header className={classes.header}>
       <Container>
         <Group justify="space-between">
-        <Group>
-          <Image
-            src="/easy-bullet-journal.svg"
-            alt="Easy Bullet Journal"
-            height={32}
-          />
-          <Text>Easy Bullet Journal</Text>
-        </Group>
-        <Group gap={5}>
-          <Button
-            onClick={() => toggleColorScheme()}
-            variant="subtle"
-            size="xs"
-            color={computedColorScheme === "dark" ? "orange" : "blue"}
-          >
-            {computedColorScheme === "dark" ? <IconSun /> : <IconMoon />}
-          </Button>
+          <Group>
+            <Image
+              src="/easy-bullet-journal.svg"
+              alt="Easy Bullet Journal"
+              height={32}
+            />
+            <Text>Easy Bullet Journal</Text>
+          </Group>
+          <Group gap={5}>
+            <Button
+              onClick={() => toggleColorScheme()}
+              variant="subtle"
+              size="xs"
+              color={computedColorScheme === "dark" ? "orange" : "blue"}
+            >
+              {computedColorScheme === "dark" ? <IconSun /> : <IconMoon />}
+            </Button>
           </Group>
         </Group>
       </Container>
